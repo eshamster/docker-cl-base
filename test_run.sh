8cffbf9c7509dd70e229bbfa69afea7a94435d4a
eshamster <hamgoostar@gmail.com>
2017-10-14 23:55:51 +0900

    Add no_cache option to test script

--- test_run.sh
+++ test_run.sh
@@ -8,5 +8,9 @@ ver=2.2
 docker rmi $(docker images | awk '/^<none>/ { print $3 }') || echo "ignore rmi error"
 docker rm `docker ps -a -q` || echo "ignore rm error"
 
-docker build -t ${name}:${ver} -t  ${name}:latest .
+no_cache=
+if [ $# -gt 0 ]; then
+    no_cache="--no-cache"
+fi
+docker build ${no_cache} -t ${name}:${ver} -t  ${name}:latest .
 docker run --name cl-test -v /tmp/temp-mount:/tmp/temp -it ${name}:${ver} /bin/sh
